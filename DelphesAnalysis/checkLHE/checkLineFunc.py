b73c51e715e7486618ee09cf4dd36d09dbcd9b2e
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-03-30 17:28:51 +0200

    Useful function for checking string in python

---
+++ DelphesAnalysis/checkLHE/checkLineFunc.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os, re, sys, shutil
+import math, ROOT
+import numpy
+
+def isComment(line):
+	l = line.strip() # Remove whitespace in begion and end of line
+	return l.startswith('#')
+
+def isEmpty(line):
+	l = line.strip() # Remove whitespace in begion and end of line
+	if l:
+		return False
+	else:
+		return True
+
