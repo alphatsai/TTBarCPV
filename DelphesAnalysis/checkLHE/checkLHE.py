7869537e5b343225b6b5a20c35beef283d68a113
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-03-30 17:29:50 +0200

    Add loading decay list

--- DelphesAnalysis/checkLHE/checkLHE.py
+++ DelphesAnalysis/checkLHE/checkLHE.py
@@ -3,38 +3,34 @@ import os, re, sys, shutil
 import math, ROOT
 import numpy
 
-RunNo = 0
-LumiNo = 1
-EvtNo = 2
+if sys.argv[1] == '':
+	print ""
+	print sys.argv[0]
 
-outfile = open('SameEventsWithBprimeTobH.txt', 'w')
+##### * My function * #####
+def isComment(line):
+	l = line.strip() # Remove whitespace in begion and end of line
+	return l.startswith('#')
 
-bHFile = 'BprimeTobH_Events'
-allFile = ['B2G_12_019_data', 'BprimeTobZ_Events',  'MultileptonEventList',  'SS2L_Event_lists']
-#allFile = ['SS2L_Event_lists']
-f_bH = open(str(bHFile+'.txt'), 'r')
-lines_bH = filter(None, (line.rstrip() for line in f_bH)) #Ingore empty line
-print '[RunNo, LumiNo, EvtNo]'
-outfile.write('RunNo\tLumiNo\tEvtNo') 
-for fileName in allFile:
-	print 'Looking for same events in '+str(fileName+".txt")+' with '+str(bHFile+'.txt')+'...'
-	outfile.write('\n') 
-	outfile.write('Comparing with '+str(fileName+".txt")+'\n') 
-	#result = numpy.loadtxt(open(str(fileName+".txt"),"r"),delimiter=" ",skiprows=1).astype('int')
-	#result.sort()
-	#print result 
-	with open(str(fileName+".txt")) as f_in:
-		lines = filter(None, (line.rstrip() for line in f_in)) #Ingore empty line
-		for line in lines:
-			if ("Lumi" or "No" or "no" or "num") in line: continue 
-			BpToOtherEvt = line.split() # [RunNo, LumiNo, EvtNo]
-			#BpToOtherEvt = map(int, BpToOtherEvt) #string to int
-			#BpToOtherEvt.sort() #For fun
-			for line_bH in lines_bH: 	
-				BpTobHEvt = line_bH.split()
-				#BpTobHEvt = map(int, BpTobHEvt)
-				if ((  BpToOtherEvt[RunNo] == BpTobHEvt[RunNo] ) 
-				and ( BpToOtherEvt[LumiNo] == BpTobHEvt[LumiNo])
-				and ( BpToOtherEvt[EvtNo]  == BpTobHEvt[EvtNo] )):  
-					print '[ '+BpTobHEvt[RunNo]+' '+BpTobHEvt[LumiNo]+' '+BpTobHEvt[EvtNo]+' ]'	
-					outfile.write(BpTobHEvt[RunNo]+'\t'+BpTobHEvt[LumiNo]+'\t'+BpTobHEvt[EvtNo]+'\n')
+def isEmpty(line):
+	l = line.strip() # Remove whitespace in begion and end of line
+	if l:
+		return False
+	else:
+		return True
+	
+##### * Main Class * #####
+decayTable = open('decayList.txt', 'r')
+#decayTable = open('test.txt', 'r')
+PIDs={}
+for line in decayTable:
+	l = line.strip() 				# Remove whitespace in begion and end of line
+	if isComment(l) or isEmpty(l):  # Remove comment and empty line
+		continue
+	ls = l.split()
+	if len(ls)  == 12: 
+		PIDs[int(ls[0])]=ls[2]
+		antiPID=-1*int(ls[0])
+		PIDs[antiPID]=ls[3]
+
+def 
