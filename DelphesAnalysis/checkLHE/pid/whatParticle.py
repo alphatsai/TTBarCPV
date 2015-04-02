ff4ad9155badfc3de88980c1ce0cedddbddf1d82
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-04-02 14:16:52 +0200

    Remove numpy

--- DelphesAnalysis/checkLHE/pid/whatParticle.py
+++ DelphesAnalysis/checkLHE/pid/whatParticle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os, re, sys, shutil
 import math, ROOT
-import numpy
 
 from optparse import OptionParser
 from PIDs import PIDs  
--- DelphesAnalysis/checkLHE/readLHE/evtInfo.py
+++ DelphesAnalysis/checkLHE/readLHE/evtInfo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os, re, sys, shutil
 import math, ROOT
-import numpy
 
 sys.path.insert(1,os.path.dirname(os.path.abspath(__file__))+'/../')
 from functions import beautyLine
