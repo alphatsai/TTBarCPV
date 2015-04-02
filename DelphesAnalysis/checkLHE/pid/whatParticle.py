ba08832bd44e8013818a0bfc91f9612817382577
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-04-02 13:42:35 +0200

    Remove unused comments

--- DelphesAnalysis/checkLHE/pid/whatParticle.py
+++ DelphesAnalysis/checkLHE/pid/whatParticle.py
@@ -17,16 +17,11 @@ usage = """
 parser = OptionParser(usage=usage)
 parser.add_option("-p", "--pid", dest="pid", type="int", #action="store", #default=0
                   help="Input particle ID")
-parser.add_option("-n", "--name", dest="name", #action="store", #default=0
+parser.add_option("-n", "--name", dest="name", 
                   help="Input particle name based on pythia table")
 parser.add_option("-d", "--data", dest="data", default='decayList.txt',
                   help="Input particle decay list from pythia")
-#parser.add_option("-l", "--lib", dest="libaries", default='../',
-#                  help="Input path of main libaries")
 (options, args) = parser.parse_args()
-
-#sys.path.append(options.libaries)
-#from PIDs import PIDs  
 
 # Main progress
 if not options.pid and not options.name:
@@ -35,13 +30,9 @@ if not options.pid and not options.name:
 pids = PIDs(options.data)
 pids.loadDecayList()
 if options.pid:
-	#print "|"
 	print ">> Paritcle ID   : "+str(options.pid)+" = "+pids.showName(options.pid)
-	#print "|"
 if options.name:
-	#print "|"
 	print ">> Paritcle name : "+pids.correctName(options.name)+" = "+str(pids.showPID(options.name))
-	#print "|"
 	
 
 #print "|"
