6f0c250a39f72fc2407dcf8ab183eb4c4f8a2519
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-04-14 14:40:02 +0200

    Debug in print out

--- DelphesAnalysis/src/argvOpts.C
+++ DelphesAnalysis/src/argvOpts.C
@@ -2,24 +2,24 @@
 bool argvOpts( char *argv[] )
 {
 	bool isVal=false;
-    if( argv[1] == NULL ){
-        printf(">> Please input file.\n"); 
+	if( argv[1] == NULL ){
+		printf(">> Please input file.\n"); 
 		printf(">> Ex: %s tag_1_delphes_events.root\n", argv[0]);
-        isVal=false;
-    }else{
+		isVal=false;
+	}else{
 		printf(">> Loaded file: %s\n", argv[1]);
-        isVal=true;
+		isVal=true;
 	}
 	if( isVal ){
-	    if( argv[2] == NULL ){
+		if( argv[2] == NULL ){
 			argv[2]="result.root";
-	   	    isVal=true;
+			isVal=true;
 		}else{
 			std::string str(argv[2]);
 			if( str.find(".root") == std::string::npos){ 
 				printf(">> [ERROR]: \"%s\" is not root file for output\n", argv[2]);
 				printf(">> [ERROR]: Please do like: %s %s result.root\n", argv[0], argv[1]);
-        		isVal=false;
+				isVal=false;
 			}
 		}
 	}
