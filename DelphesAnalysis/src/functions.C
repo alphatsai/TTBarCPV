58af13746a4d1c4cbd43c25dd568eb22076a6fad
Jui-Fa Tsai <jui-fa.tsai@xern.ch>
2015-04-14 17:48:59 +0200

    Cleaning up the functions of 3D vector

--- DelphesAnalysis/src/functions.C
+++ DelphesAnalysis/src/functions.C
@@ -33,108 +33,43 @@ GenParticle *getMatchedGenParticle(Jet *jet, TClonesArray *branchParticle, int P
 }
 
 //3D vector functions
-double dotP3( TLorentzVector v1, TLorentzVector v2)
+double dotP3( double x1, double y1, double z1, double x2, double y2, double z2 )
 {
-	return v1.Px()*v2.Px()+v1.Py()*v2.Py()+v1.Pz()*v2.Pz();
+	return (x1*x2)+(y1*y2)+(z1*z2);
 }
-double dotP3( vector<double> v1, vector<double> v2)
-{
-	return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
-}
-double dotP3( TLorentzVector v1, vector<double> v2)
-{
-	return v1.Px()*v2[0]+v1.Py()*v2[1]+v1.Pz()*v2[2];
-}
-double dotP3( vector<double> v1, TLorentzVector v2)
-{
-	return v2.Px()*v1[0]+v2.Py()*v1[1]+v2.Pz()*v1[2];
-}
-vector<double> crossP3( TLorentzVector v1, TLorentzVector v2)
-{
-	vector<double> product;
-	product.push_back( v1.Py()*v2.Pz()-v1.Pz()*v2.Py() ); //x
-	product.push_back( v1.Pz()*v2.Px()-v1.Px()*v2.Pz() ); //y
-	product.push_back( v1.Px()*v2.Py()-v1.Py()*v2.Px() ); //z 	
-	return product;
-}
-vector<double> crossP3(  vector<double> v1, vector<double> v2)
-{
-	vector<double> product;
-	product.push_back( v1[1]*v2[2]-v1[2]*v2[1] ); //x
-	product.push_back( v1[2]*v2[0]-v1[0]*v2[2] ); //y
-	product.push_back( v1[0]*v2[1]-v1[1]*v2[0] ); //z 	
-	return product;
-}
-vector<double> crossP3( vector<double> v1, TLorentzVector v2)
-{
-	vector<double> product;
-	product.push_back( v1[1]*v2.Pz()-v1[2]*v2.Py() ); //x
-	product.push_back( v1[2]*v2.Px()-v1[0]*v2.Pz() ); //y
-	product.push_back( v1[0]*v2.Py()-v1[1]*v2.Px() ); //z 	
-	return product;
-}
-vector<double> crossP3(  TLorentzVector v1, vector<double> v2)
-{
-	vector<double> product;
-	product.push_back( v1.Py()*v2[2]-v1.Pz()*v2[1] ); //x
-	product.push_back( v1.Pz()*v2[0]-v1.Px()*v2[2] ); //y
-	product.push_back( v1.Px()*v2[1]-v1.Py()*v2[0] ); //z 	
-	return product;
-}
+double dotP3( TLorentzVector v1, TLorentzVector v2 ){ return dotP3( v1.Px(), v1.Py(), v1.Pz(), v2.Px(), v2.Py(), v2.Pz() ); }
+double dotP3( TLorentzVector v1, vector<double> v2 ){ return dotP3( v1.Px(), v1.Py(), v1.Pz(), v2[0], v2[1], v2[2] ); }
+double dotP3( vector<double> v1, TLorentzVector v2 ){ return dotP3( v1[0], v1[1], v1[2], v2.Px(), v2.Py(), v2.Pz() ); }
+double dotP3( vector<double> v1, vector<double> v2 ){ return dotP3( v1[0], v1[1], v1[2], v2[0], v2[1], v2[2] ); }
 
-vector<double> addP3( TLorentzVector v1, TLorentzVector v2, int isSubtract=0 )
+vector<double> crossP3( double x1, double y1, double z1, double x2, double y2, double z2)
+{
+	vector<double> product;
+	product.push_back( (y1*z2)-(z1*y2) ); //x
+	product.push_back( (z1*x2)-(x1*z2) ); //y
+	product.push_back( (x1*y2)-(y1*x2) ); //z 	
+	return product;
+}
+vector<double> crossP3( TLorentzVector v1, TLorentzVector v2 ){ return crossP3( v1.Px(), v1.Py(), v1.Pz(), v2.Px(), v2.Py(), v2.Pz() ); }
+vector<double> crossP3( TLorentzVector v1, vector<double> v2 ){ return crossP3( v1.Px(), v1.Py(), v1.Pz(), v2[0], v2[1], v2[2] ); }
+vector<double> crossP3( vector<double> v1, TLorentzVector v2 ){ return crossP3( v1[0], v1[1], v1[2], v2.Px(), v2.Py(), v2.Pz() ); }
+vector<double> crossP3( vector<double> v1, vector<double> v2 ){	return crossP3( v1[0], v1[1], v1[2], v2[0], v2[1], v2[2] ); }
+
+vector<double> addP3( double x1, double y1, double z1, double x2, double y2, double z2, int isSubtract=0 )
 {
 	vector<double> newP3;
 	if( isSubtract < 0 ){
-		newP3.push_back( v1.Px()-v2.Px() );	
-		newP3.push_back( v1.Py()-v2.Py() );	
-		newP3.push_back( v1.Pz()-v2.Pz() );	
+		newP3.push_back( x1-x2 );	
+		newP3.push_back( y1-y2 );	
+		newP3.push_back( z1-z2 );	
 	}else{
-		newP3.push_back( v1.Px()+v2.Px() );	
-		newP3.push_back( v1.Py()+v2.Py() );	
-		newP3.push_back( v1.Pz()+v2.Pz() );	
+		newP3.push_back( x1+x2 );	
+		newP3.push_back( y1+y2 );	
+		newP3.push_back( z1+z2 );	
 	}
 	return newP3;
 }
-vector<double> addP3( vector<double> v1, vector<double> v2, int isSubtract=0 )
-{
-	vector<double> newP3;
-	if( isSubtract < 0 ){
-		newP3.push_back( v1[0]-v2[0] );	
-		newP3.push_back( v1[1]-v2[1] );	
-		newP3.push_back( v1[2]-v2[2] );	
-	}else{
-		newP3.push_back( v1[0]+v2[0] );	
-		newP3.push_back( v1[1]+v2[1] );	
-		newP3.push_back( v1[2]+v2[2] );	
-	}
-	return newP3;
-}
-vector<double> addP3( TLorentzVector v1, vector<double> v2, int isSubtract=0 )
-{
-	vector<double> newP3;
-	if( isSubtract < 0 ){
-		newP3.push_back( v1.Px()-v2[0] );	
-		newP3.push_back( v1.Py()-v2[1] );	
-		newP3.push_back( v1.Pz()-v2[2] );	
-	}else{                             
-		newP3.push_back( v1.Px()+v2[0] );	
-		newP3.push_back( v1.Py()+v2[1] );	
-		newP3.push_back( v1.Pz()+v2[2] );	
-	}
-	return newP3;
-}
-vector<double> addP3( vector<double> v1, TLorentzVector v2, int isSubtract=0 )
-{
-	vector<double> newP3;
-	if( isSubtract < 0 ){
-		newP3.push_back( v1[0]-v2.Px() );	
-		newP3.push_back( v1[1]-v2.Py() );	
-		newP3.push_back( v1[2]-v2.Pz() );	
-	}else{                             
-		newP3.push_back( v1[0]+v2.Px() );	
-		newP3.push_back( v1[1]+v2.Py() );	
-		newP3.push_back( v1[2]+v2.Pz() );	
-	}
-	return newP3;
-}
+vector<double> addP3( TLorentzVector v1, TLorentzVector v2, int isSubtract=0 ){ return addP3( v1.Px(), v1.Py(), v1.Pz(), v2.Px(), v2.Py(), v2.Pz(), isSubtract ); }
+vector<double> addP3( TLorentzVector v1, vector<double> v2, int isSubtract=0 ){ return addP3( v1.Px(), v1.Py(), v1.Pz(), v2[0], v2[1], v2[2], isSubtract ); }
+vector<double> addP3( vector<double> v1, TLorentzVector v2, int isSubtract=0 ){ return addP3( v1[0], v1[1], v1[2], v2.Px(), v2.Py(), v2.Pz(), isSubtract ); }
+vector<double> addP3( vector<double> v1, vector<double> v2, int isSubtract=0 ){ return addP3( v1[0], v1[1], v1[2], v2[0], v2[1], v2[2], isSubtract ); }
